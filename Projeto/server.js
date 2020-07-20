f5addb8fab604a779ca89e16264d56668f5f5a57
Natan Balthazar <balthazarnatan@gmail.com>
2020-07-20 03:17:35 -0300

    Atualizações da Aula 4
    Lista do que foi feito:
    Criação do Back-End;
    Atualização de Pastas;
    Organização e Limpeza do Código;
    Criação de um servidor a parte;

--- Projeto/server.js
+++ Projeto/server.js
@@ -1,1 +1,77 @@
-console.log("Oi tudo bem?")
+const express = require("express")
+const server = express()
+
+const ideas = [
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+        title: "Cusos de Programação",
+        category: "Estudo",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, quam?",
+        url: "http://rocketseat.com.br",
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+        title: "Exercicios",
+        category: "Saude",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, quam?",
+        url: "http://rocketseat.com.br",
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+        title: "Meditação",
+        category: "Mentalidade",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, quam?",
+        url: "http://rocketseat.com.br",
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+        title: "Karaokê",
+        category: "Diversão em Família",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, quam?",
+        url: "http://rocketseat.com.br",
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729038.svg",
+        title: "Pintura",
+        category: "Criatividade",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, quam?",
+        url: "http://rocketseat.com.br",
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729048.svg",
+        title: "Recortes",
+        category: "Criatividade",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, quam?",
+        url: "http://rocketseat.com.br",
+    },
+]
+
+server.use(express.static("public"))
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("views", {
+    express: server,
+    noCache: true,
+})
+
+server.get("/", function(req, res){
+
+    const reversedIdeas = [...ideas].reverse()
+
+    let lastIdeias = []
+    for (let idea of reversedIdeas){
+        if(lastIdeias.length < 2){
+            lastIdeias.push(idea)
+        }
+    }
+    return res.render("index.html", { ideas : lastIdeias })
+})
+
+server.get("/ideias", function(req, res){
+
+    const reversedIdeas = [...ideas].reverse()
+
+    return res.render("ideias.html", { ideas: reversedIdeas})
+})
+
+server.listen(3000)
