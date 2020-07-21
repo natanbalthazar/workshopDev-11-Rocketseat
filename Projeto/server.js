1cefce00b95cf5caa177a7f5a8fbc77aef709811
Natan Balthazar <balthazarnatan@gmail.com>
2020-07-20 23:30:13 -0300

    Alterações Inicias da aula 5
    Lista do que foi feito:
    Criação do Banco de dados
    Configuração do Servidor SQLite
    Alterações do código em alguns pontos

--- Projeto/server.js
+++ Projeto/server.js
@@ -1,77 +1,90 @@
+//Utilização do expressa para criar e configurar o Servidor
 const express = require("express")
 const server = express()
 
-const ideas = [
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-        title: "Cusos de Programação",
-        category: "Estudo",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, quam?",
-        url: "http://rocketseat.com.br",
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
-        title: "Exercicios",
-        category: "Saude",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, quam?",
-        url: "http://rocketseat.com.br",
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
-        title: "Meditação",
-        category: "Mentalidade",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, quam?",
-        url: "http://rocketseat.com.br",
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
-        title: "Karaokê",
-        category: "Diversão em Família",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, quam?",
-        url: "http://rocketseat.com.br",
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729038.svg",
-        title: "Pintura",
-        category: "Criatividade",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, quam?",
-        url: "http://rocketseat.com.br",
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729048.svg",
-        title: "Recortes",
-        category: "Criatividade",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, quam?",
-        url: "http://rocketseat.com.br",
-    },
-]
+const db = require("./db")
 
+//Configuração de arquivos estáticos (CSS, Scripts, Imagens)
 server.use(express.static("public"))
 
+//habilitar uso do req.body
+server.use(express.urlencoded({ extended: true }))
+
+//Configurações do Nunjucks
 const nunjucks = require("nunjucks")
 nunjucks.configure("views", {
     express: server,
     noCache: true,
 })
 
+//Criação de rotas /
+// Capturação do Pedido do Cliente para responder
 server.get("/", function(req, res){
 
-    const reversedIdeas = [...ideas].reverse()
+    db.all(`SELECT * FROM ideas`, function(err, rows){
+            if (err) {
+                console.log(err)
+                return res.send("Erro no banco de dados!")
+            }
+    
+            const reversedIdeas = [...rows].reverse()
 
-    let lastIdeias = []
-    for (let idea of reversedIdeas){
-        if(lastIdeias.length < 2){
-            lastIdeias.push(idea)
-        }
-    }
-    return res.render("index.html", { ideas : lastIdeias })
-})
+            let lastIdeias = []
+            for (let idea of reversedIdeas){
+                if(lastIdeias.length < 2){
+                    lastIdeias.push(idea)
+                }
+            }
+            return res.render("index.html", { ideas : lastIdeias })
+        })
+
+})   
 
 server.get("/ideias", function(req, res){
 
-    const reversedIdeas = [...ideas].reverse()
+    db.all(`SELECT * FROM ideas`, function(err, rows){
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
+        }
 
-    return res.render("ideias.html", { ideas: reversedIdeas})
+        const reversedIdeas = [...rows].reverse()
+    
+        return res.render("ideias.html", { ideas: reversedIdeas})
+    })
+
 })
 
+//Inserir dados na tabela
+server.post("/", function(req, res){
+    const query = `
+        INSERT INTO ideas(
+            image,
+            title,
+            category,
+            description,
+            link
+        ) VALUES (?,?,?,?,?);
+    `
+ 
+    const values = [
+        req.body.image,
+        req.body.title,
+        req.body.category,
+        req.body.description,
+        req.body.link,
+    ]
+
+
+    db.run(query, values, function(err) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
+        }
+
+        return res.redirect("/ideias")
+    })
+})
+
+// Ligação do Servidor na Porta 3000
 server.listen(3000)
--- projeto/public/scripts.js
+++ projeto/public/scripts.js
@@ -14,3 +14,4 @@ function onOff(){
         .classList
         .toggle("addScroll")
 }
+
